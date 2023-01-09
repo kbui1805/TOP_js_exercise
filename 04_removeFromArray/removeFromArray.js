058ac84158fbdbb2808a0e71fcdd47a3b7fc5e5b
Khanh Bui <khanhbui.work@gmail.com>
2023-01-08 22:44:59 -0500

    Pass Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(main_arr, ...num_remove) {
+    let result = [];
+    main_arr.map((main_element) => {
+        let isRemove = num_remove.filter((num) => {
+            return (num === main_element);
+        }).length;
+        if (!isRemove) {result.push(main_element)}
+    });
+    return result;
 };
 
 // Do not edit below this line
