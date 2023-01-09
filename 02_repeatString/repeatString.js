3f88d25d5f1ed5cdc562148bfd5f20541c031fbc
Khanh Bui <khanhbui.work@gmail.com>
2023-01-08 21:44:57 -0500

    Pass Exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, num_repeat) {
+    let result = '';
+    let i = 0;
+    if (num_repeat < 0) {
+        result = 'ERROR';
+    }
 
+    while(num_repeat && num_repeat > i ) {
+        result += word;
+        i++;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
