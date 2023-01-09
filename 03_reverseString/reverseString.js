a789687f6a91d03acdcf5273bd5074af4eef5855
Khanh Bui <khanhbui.work@gmail.com>
2023-01-08 22:22:19 -0500

    Pass Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result = '';
+    let str_split = str.split('');
+    for (let char of str_split.reverse()) {
+        result+= char;
+    }
+    return result;
 };
 
 // Do not edit below this line
