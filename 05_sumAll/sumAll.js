8d309b04967f4fe26b8e8bce7b36a1912bf80ee2
Khanh Bui <khanhbui.work@gmail.com>
2023-01-10 22:31:37 -0500

    Pass Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num_start, num_end) {
 
+    if((Number.isInteger(num_start) && num_start >= 0) && (Number.isInteger(num_end) && num_end >= 0)) {
+        let result = num_end;
+        let next = (() => { return num_start < num_end ? 1 : -1})();
+        let i = num_start;
+
+        if(num_start == num_end) {
+            return num_start;
+        }
+
+        while (i != num_end) {
+            result = result + i;
+            i+=next;
+        }
+        
+        return result;
+    }
+
+    return "ERROR";
+
+    
 };
+
+// sumAll(1, 4);
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
