67514e163edf0dbaa0817882f0509ffb3d5beec2
Khanh Bui <khanhbui.work@gmail.com>
2023-01-11 13:28:19 -0500

    Pass Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(temp_in_F) {
+  let result = Number.parseFloat(((temp_in_F - 32) * 5/9));
 
-const convertToFahrenheit = function() {
+  if (result / Math.floor(result) != 0){
+    return Number.parseFloat(result.toFixed(1));
+  }
+
+  return result;
+};  
+
+const convertToFahrenheit = function(temp_in_C) {
+  let result = Number.parseFloat(((temp_in_C * 9/5) + 32));
+
+  if (result / Math.floor(result) != 0){
+    return Number.parseFloat(result.toFixed(1));
+  }
+
+  return result;
 };
 
 // Do not edit below this line
