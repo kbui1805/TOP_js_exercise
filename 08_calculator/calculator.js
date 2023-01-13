3c2d5015c42f71c366d0740ebf6d57f72c3bba99
Khanh Bui <khanhbui.work@gmail.com>
2023-01-13 13:21:53 -0500

    Pass Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let result;
+  result = num1 + num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let result;
+  result = num1 - num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(sum_list) {
+	let result = 0;
+  sum_list.map(num => {
+    result += num;
+  });
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(mul_list) {
+  let result = 0;
+  if (mul_list.length > 0) {
+    result = 1;
+    mul_list.map(num => {
+      result *= num;
+    });
+  }
+  return result;
 
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	let result = 1;
+  let i = pow;
+  while(i > 0){
+    result *= num;
+    i--;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  if(num > 1){
+    let i = 1;
+    while (i <= num) {
+      result *= i;
+      i++;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
