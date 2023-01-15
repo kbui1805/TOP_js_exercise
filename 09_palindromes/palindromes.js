69cf06010661756bc2b8ee7f023060b6468bbb07
Khanh Bui <khanhbui.work@gmail.com>
2023-01-15 12:33:58 -0500

    Pass Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (word_test) {
+    let result = false;
+    let word_test_arr = word_test.split("")
+    .filter((char) => {
+        let pattern = /\w/;
+        return pattern.test(char);
+    })
+    .map((char) => {
+        return char.toLowerCase();
+    });
 
+    let original_word = word_test_arr.join('');
+    let reverse_word = (() => {
+        let reverse_arr = [];
+        word_test_arr.map((char) => {
+            reverse_arr.unshift(char);
+        });
+        return reverse_arr;
+    })().join('');
+
+    if(original_word === reverse_word) {
+        result = true;
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
