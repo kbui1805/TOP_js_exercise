b59d4e4201e06536f421509cb7739ed34e7a917c
Khanh Bui <khanhbui.work@gmail.com>
2023-01-16 13:41:20 -0500

    Pass Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let result;
+    let formated_num = Number.parseInt(num);
+    let arr = [1, 1];
+    if (formated_num <= 0) {
+        result = 'OOPS';
+    }
+    if (formated_num == 1 || num == 2) {
+        result = 1;
+    }
+    if (formated_num > 2) {
+        let i = 2
+        while(i <= formated_num) {
+            let new_fibo_num = arr[arr.length - 1] + arr[arr.length - 2];
+            arr.push(new_fibo_num);
+            i++
+        }
+        result = arr[formated_num - 1];
+    }
+
+    return result;
 
 };
 
