f0309314e822891dc76fe70ad44479a17fb5950a
Khanh Bui <khanhbui.work@gmail.com>
2023-01-17 13:19:41 -0500

    Pass Exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books_list) {
+    let title_list = [];
+    for (let {title} of books_list) {
+        title_list.push(title);
+    }
+    return title_list;
 };
 
 // Do not edit below this line
