0f9162d199b56195c780a8053a601a7f0f1e996d
Khanh Bui <khanhbui.work@gmail.com>
2023-01-20 22:36:43 -0500

    Pass Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people_list) {
+    let oldest_person;
+    let current_oldest_age = 0;
+    function getAge(person) {
+        let person_age;
+        if (person.hasOwnProperty('yearOfDeath')) {
+            person_age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            let currentYear = new Date();
+            person_age = currentYear.getFullYear() - person.yearOfBirth;
+        }
+        return person_age;
+    }
 
+    for (let people of people_list) {
+        if (getAge(people) > current_oldest_age) {
+            let result_age = getAge(people);
+            oldest_person = people;
+            current_oldest_age = result_age;
+        }
+    }
+
+    return oldest_person;
 };
 
 // Do not edit below this line
